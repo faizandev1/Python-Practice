eee56df3e6b5905a3a7bce302aa046bd9b9040da
itsmefaizan <lanser2676@gmail.com>
2026-02-14 15:47:24 +0500

    Add all project files fresh

---
+++ practice.py
@@ -0,0 +1,5 @@
+print("My name is Faizan ")
+
+a, b = 5, 10
+sum = a + b
+print("The sum of a and b is:", sum)
---
+++ typecasting.py
@@ -0,0 +1,17 @@
+a=5
+b=7.5
+
+print("A IS  :"+ str(type(a)))
+print("B IS  :"+ str(type(b)))
+
+# There is two type of typecasting
+# 1. Implicit Typecasting(as me doing by it self to a program )
+c="34"
+f="34"
+print(int(c)+int(f))
+
+
+# 2. Explicit Typecasting(python automatically typecast it self )
+r=4
+g=5.6
+print(r+g)
